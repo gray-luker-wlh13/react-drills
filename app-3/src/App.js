75f8c5be96a83ab1cd54be49d3f83e197cd50167
grayluker05 <graysonluker@gmail.com>
2019-11-13 14:01:53 -0700

    finished app3 twice

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      filterStr: '',
+      friends: ['Reese', 'Maxwell', 'Christian']
+    }
+  }
+
+  handleChange = (filter) => {
+    this.setState({
+      filterStr: filter
+    })
+  }
+
   render() {
+    let filteredFriends = this.state.friends.filter(e => {
+      return e.includes(this.state.filterStr)
+    }).map((e, i) => {
+      return <h2 key={i}>{e}</h2>
+    })
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={(e) => this.handleChange(e.target.value)}/>
+        {filteredFriends}
       </div>
     );
   }
